f66e25abef08c23e4935cdab48eb6a3ddd8a4a83
Andrei Hnedchik <andrey.gnedchic@gmail.com>
2024-06-30 00:20:26 +0300

    add host

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = local_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = local_settings.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['AndreyG.pythonanywhere.com']
 
 
 # Application definition
