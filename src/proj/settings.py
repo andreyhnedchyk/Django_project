4d9b27051b807cedff25f1437c90d9060886dbca
Andrei Hnedchik <andrey.gnedchic@gmail.com>
2024-06-30 01:44:28 +0300

    add static and media

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -115,8 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_URL_ROOT ='/home/AndreyG/static'
+MEDIA_ROOT = '/home/AndreyG/media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
